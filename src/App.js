3651c6f2b8d90a6f39c03e888ff509074b7e4a85
Yvonne Nkr <yvonnkr86@gmail.com>
2020-02-24 16:09:01 +0000

    setup pages/components folders and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import PageNotFound from "./shared/components/404";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/places/new">
+          <NewPlace />
+        </Route>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/404">
+          <PageNotFound />
+        </Route>
+
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewPlace = () => {
+  return (
+    <div>
+      <h1>New place</h1>
+    </div>
+  );
+};
+
+export default NewPlace;
