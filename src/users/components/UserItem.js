12f17e5f6ad3e4ec40562f42d8a9cab2afa96182
Yvonne Nkr <yvonnkr86@gmail.com>
2020-02-24 16:35:55 +0000

    added userslist page/component

---
+++ src/users/components/UserItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserItem = () => {
+  return (
+    <div>
+      <h3>User item</h3>
+    </div>
+  );
+};
+
+export default UserItem;
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,9 +1,22 @@
 import React from "react";
+import UsersList from "./../components/UsersList";
 
 const Users = () => {
+  //dummy users --will later fetch from backend
+  const USERS = [
+    {
+      id: "u1",
+      name: "John Smith",
+      image:
+        "https://www.planetware.com/photos-large/MEX/mexico-top-places-cancun-mayan-riviera.jpg",
+      places: 3
+    }
+  ];
+
   return (
     <div>
       <h1>Users page</h1>
+      <UsersList items={USERS} />
     </div>
   );
 };
