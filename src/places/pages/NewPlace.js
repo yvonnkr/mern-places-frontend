55ad7c56551e80fa88b888383e3d48f992afdb70
Yvonne Nkr <yvonnkr86@gmail.com>
2020-02-25 17:29:08 +0000

    Added custom Input component and state management

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import Input from "./../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
 const NewPlace = () => {
   return (
-    <div>
-      <h1>New place</h1>
-    </div>
+    <form className="place-form">
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[]}
+        errorText="Please enter a valid title"
+      />
+    </form>
   );
 };
 
