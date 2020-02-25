bcdfd052b0b6da65edf7305cd9b228c79297488b
Yvonne Nkr <yvonnkr86@gmail.com>
2020-02-25 16:07:53 +0000

    Render map with google map / added env var

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "./../../shared/components/UIElements/Modal/Modal";
+import Map from "./../../shared/components/UIElements/Map/Map";
 import "./PlaceItem.css";
 
 const PlaceItem = props => {
@@ -33,7 +34,7 @@ const PlaceItem = props => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP</h2>
+          <Map center={coordinates} zoom={16} />
         </div>
       </Modal>
       <li className="place-item">
