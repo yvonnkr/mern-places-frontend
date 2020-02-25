a3c2aa301827b874a1478cf4d51c314b1e206a14
Yvonne Nkr <yvonnkr86@gmail.com>
2020-02-25 19:35:08 +0000

    Added form submit functionality

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -56,8 +56,15 @@ const NewPlace = () => {
     });
   }, []);
 
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+
+    //later send data to backend/server
+    console.log(formState.inputs);
+  };
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -73,6 +80,14 @@ const NewPlace = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
