318d237d4740f8688800fe2e9c53b3aa35c15fb8
Yvonne Nkr <yvonnkr86@gmail.com>
2020-02-26 20:46:43 +0000

    Added AuthContext for app-wide state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -13,36 +13,51 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./users/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
-  return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId" exact>
-            <UpdatePlace />
-          </Route>
-          <Route path="/auth" exact>
-            <Auth />
-          </Route>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/404">
-            <PageNotFound />
-          </Route>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-          <Redirect to="/404" />
-        </Switch>
-      </main>
-    </Router>
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/:userId/places" exact>
+              <UserPlaces />
+            </Route>
+            <Route path="/places/new" exact>
+              <NewPlace />
+            </Route>
+            <Route path="/places/:placeId" exact>
+              <UpdatePlace />
+            </Route>
+            <Route path="/auth" exact>
+              <Auth />
+            </Route>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/404">
+              <PageNotFound />
+            </Route>
+
+            <Redirect to="/404" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
