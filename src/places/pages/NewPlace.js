9d55831a3a4fa076fa20f8af1cb50e197942c368
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-07 14:00:42 +0000

    fixed some minor issues

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -52,8 +52,8 @@ const NewPlace = () => {
       formData.append("title", formState.inputs.title.value);
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
-      formData.append("creator", auth.userId);
       formData.append("imageUrl", formState.inputs.image.value);
+      // formData.append("creator", auth.userId);
 
       await sendRequest("http://localhost:5000/api/places", "POST", formData, {
         Authorization: `Bearer ${auth.token}`
